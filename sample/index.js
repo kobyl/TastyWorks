5a1057b7dfde214108fea6ef871d1553433fe422
kobyl <koby.leung@lexisnexis.com>
2021-01-11 12:50:32 -0800

    Start writing models

--- sample/index.js
+++ sample/index.js
@@ -1,10 +1,13 @@
 'use strict';
+const fs = require('fs');
 
 const TastyWorks = require('../lib/index');
 const credentials = {
     username: process.env.TASTY_USERNAME,
     password: process.env.TASTY_PASSWORD
 };
+
+console.log(JSON.stringify(credentials));
 
 let TASTY_ACCOUNT_ID;
 
@@ -19,49 +22,55 @@ TastyWorks.authorization()
         console.log('Session is active, continue with other calls.');
         return true;
     })
-    .then(() => TastyWorks.accounts())
-    .catch(err => console.log(err.status))
-    .then(accounts => {
-        TASTY_ACCOUNT_ID = accounts[0]['account-number'];
-        TastyWorks.setUser({accounts});
-    })
-    .then(() => {
-        console.log('======= USER OBJECT =======');
-        console.log(TastyWorks.getUser());
-    })
-    .then(() => TastyWorks.balances(TASTY_ACCOUNT_ID))
-    .catch(err => console.log(err.status))
-    .then(balances => {
-        console.log('======= ACCOUNT BALANCES =======');
-        console.log(balances)
-    })
-    .then(() => TastyWorks.positions(TASTY_ACCOUNT_ID))
-    .catch(err => console.log(err.status))
-    .then(positions => {
-        console.log('======= ACCOUNT POSITIONS =======');
-        console.log(positions)
-    })
-    .then(() => TastyWorks.liveOrders(TASTY_ACCOUNT_ID))
-    .catch(err => console.log(err.status))
-    .then(liveOrders => {
-        console.log('======= ACCOUNT LIVEORDERS =======');
-        console.log(liveOrders)
-    })
-    .then(() => TastyWorks.history(TASTY_ACCOUNT_ID, '01/01/2019', '01/05/2019'))
-    .catch(err => console.log(err.status))
-    .then(history => {
-        console.log('======= ACCOUNT HISTORY =======');
-        console.log(history)
-    })
-    .then(() => TastyWorks.marketMetrics(['AMZN', 'SPX']))
-    .then(marketData => {
-        console.log('======= Market Data =======');
-        console.log(marketData)
-    })
+    // .then(() => TastyWorks.accounts())
+    // .catch(err => console.log(err.status))
+    // .then(accounts => {
+    //     TASTY_ACCOUNT_ID = accounts[0]['account-number'];
+    //     TastyWorks.setUser({accounts});
+    // })
+    // .then(() => {
+    //     console.log('======= USER OBJECT =======');
+    //     console.log(TastyWorks.getUser());
+    // })
+    // .then(() => TastyWorks.balances(TASTY_ACCOUNT_ID))
+    // .catch(err => console.log(err.status))
+    // .then(balances => {
+    //     console.log('======= ACCOUNT BALANCES =======');
+    //     console.log(balances)
+    // })
+    // .then(() => TastyWorks.positions(TASTY_ACCOUNT_ID))
+    // .catch(err => console.log(err.status))
+    // .then(positions => {
+    //     console.log('======= ACCOUNT POSITIONS =======');
+    //     console.log(positions)
+    // })
+    // .then(() => TastyWorks.liveOrders(TASTY_ACCOUNT_ID))
+    // .catch(err => console.log(err.status))
+    // .then(liveOrders => {
+    //     console.log('======= ACCOUNT LIVEORDERS =======');
+    //     console.log(liveOrders)
+    // })
+    // .then(() => TastyWorks.history(TASTY_ACCOUNT_ID, '01/01/2019', '01/05/2019'))
+    // .catch(err => console.log(err.status))
+    // .then(history => {
+    //     console.log('======= ACCOUNT HISTORY =======');
+    //     console.log(history)
+    // })
+    // .then(() => TastyWorks.marketMetrics(['AMZN', 'SPX']))
+    // .then(marketData => {
+    //     console.log('======= Market Data =======');
+    //     console.log(marketData)
+    // })
     .then(() => TastyWorks.optionChain('TSLA'))
     .then(chain => {
         console.log('======= Option chain =======');
         console.log(chain)
         console.table(chain.items[0].expirations)
+        fs.writeFile("c:\\temp\\tsla.txt", JSON.stringify(chain), (err, data) => {
+            err && console.log(err);
+            data && console.table(data);
+        });
     })
+    .then(() => TastyWorks.streamer())
+    .then((data) => TastyWorks.cometd(data));
 
